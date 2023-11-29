965d673cbb8d0923c81f2fe9d82d84aeca5ae28d
Rafal Slabon <rafal.slabon@ocado.com>
2023-11-29 09:04:27 +0100

    cleanup

--- src/aoc2023/core.clj
+++
@@ -1,6 +0,0 @@
-(ns aoc2023.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/aoc2023/core_test.clj
+++ test/aoc2023/core_test.clj
@@ -1,7 +1,6 @@
 (ns aoc2023.core-test
-  (:require [clojure.test :refer :all]
-            [aoc2023.core :refer :all]))
+  (:require [clojure.test :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
