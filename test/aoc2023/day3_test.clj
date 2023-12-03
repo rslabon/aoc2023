973e0a2893b87ab8de73b334d6d020532276cc69
rslabon <rslabon@gmail.com>
2023-12-03 12:37:55 +0100

    day3

---
+++ test/aoc2023/day3_test.clj
@@ -0,0 +1,167 @@
+(ns aoc2023.day3-test
+  (:require [clojure.string :as str]
+            [clojure.test :refer :all]))
+
+(def example-input "467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..")
+
+(def puzzle-input (slurp "resources/day3.txt"))
+
+(defn is-number?
+  [string]
+  (re-matches #"\d" string)
+  )
+
+(defn get-number-and-id
+  [grid r-idx c-idx]
+  (if (is-number? (nth (nth grid r-idx) c-idx))
+    (let [row (nth grid r-idx)
+          column-length (count row)
+          [pre pre-idx] (loop [idx c-idx
+                               s ""]
+                          (if (or (< idx 0) (not (is-number? (nth row idx))))
+                            [s (inc idx)]
+                            (recur (dec idx) (str (nth row idx) s))))
+          [post post-idx] (loop [idx (inc c-idx)
+                                 s ""]
+                            (if (or (>= idx column-length) (not (is-number? (nth row idx))))
+                              [s (dec idx)]
+                              (recur (inc idx) (str s (nth row idx)))))
+          value (str pre post)]
+      (if (empty? value)
+        nil
+        [(read-string value) (str r-idx pre-idx post-idx)]
+        ))
+    nil
+    ))
+
+(defn parse
+  [input]
+  (let [lines (vec (str/split-lines input))
+        grid (mapv #(vec (str/split % #"")) lines)
+        coords (flatten (map-indexed
+                          (fn [r-idx row]
+                            (map-indexed
+                              (fn [c-index cell]
+                                (let [[number-value number-id] (get-number-and-id grid r-idx c-index)]
+                                  {:coords [r-idx c-index],
+                                   :cell   cell,
+                                   :id     number-id
+                                   :number number-value
+                                   }
+                                  )) row)) grid))
+        coords (filter #(not= "." (:cell %)) coords)]
+    coords
+    ))
+
+(defn adj-coords-numbers
+  [numberic-by-coords symoblic-coord]
+  (let [coords (:coords symoblic-coord)
+        dx [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]]
+        adj (map #(map + coords %) dx)
+        adj-coords (filter some? (map #(get numberic-by-coords %) adj))]
+    (flatten adj-coords)))
+
+(defn get-numbers
+  [numbers-by-id]
+  (map #(:number (first (get numbers-by-id %))) (keys numbers-by-id))
+  )
+
+(defn part1
+  [input]
+  (let [coords (parse input)
+        numberic-coords (filter #(some? (:number %)) coords)
+        numberic-by-coords (group-by :coords numberic-coords)
+        symoblic-coords (filter #(nil? (:number %)) coords)
+        adj-coords (flatten (map #(adj-coords-numbers numberic-by-coords %) symoblic-coords))
+        numbers-by-id (group-by :id adj-coords)
+        numbers (get-numbers numbers-by-id)]
+    (reduce + numbers))
+  )
+
+(defn gear-ratios
+  [numberic-by-coords gear-coords]
+  (map #(let [adj-coords (adj-coords-numbers numberic-by-coords %)
+              adj-by-id (group-by :id adj-coords)
+              adj-numbers (get-numbers adj-by-id)]
+          (if (> (count adj-numbers) 1)
+            (apply * adj-numbers)
+            0
+            )) gear-coords)
+  )
+
+(defn part2
+  [input]
+  (let [coords (parse input)
+        numberic-coords (filter #(some? (:number %)) coords)
+        numberic-by-coords (group-by :coords numberic-coords)
+        gear-coords (filter #(= "*" (str (:cell %))) coords)
+        gears-ratios (gear-ratios numberic-by-coords gear-coords)]
+    (reduce + gears-ratios)
+    )
+  )
+
+(deftest day3-test
+  (testing "parse"
+    (is (= (parse "1234") [{:cell   "1"
+                            :coords [0 0]
+                            :id     "003"
+                            :number 1234}
+                           {:cell   "2"
+                            :coords [0 1]
+                            :id     "003"
+                            :number 1234}
+                           {:cell   "3"
+                            :coords [0 2]
+                            :id     "003"
+                            :number 1234}
+                           {:cell   "4"
+                            :coords [0 3]
+                            :id     "003"
+                            :number 1234}]))
+    (is (= (parse "12.") [{:cell   "1"
+                           :coords [0 0]
+                           :id     "001"
+                           :number 12}
+                          {:cell   "2"
+                           :coords [0 1]
+                           :id     "001"
+                           :number 12}]))
+    (is (= (parse "12.35") [{:cell   "1"
+                             :coords [0 0]
+                             :id     "001"
+                             :number 12}
+                            {:cell   "2"
+                             :coords [0 1]
+                             :id     "001"
+                             :number 12}
+                            {:cell   "3"
+                             :coords [0 3]
+                             :id     "034"
+                             :number 35}
+                            {:cell   "5"
+                             :coords [0 4]
+                             :id     "034"
+                             :number 35}]))
+    (is (= (parse "12.*") [{:cell   "1"
+                            :coords [0 0]
+                            :id     "001"
+                            :number 12}
+                           {:cell   "2"
+                            :coords [0 1]
+                            :id     "001"
+                            :number 12}
+                           {:cell   "*"
+                            :coords [0 3]
+                            :id     nil
+                            :number nil}]))
+    )
+  (testing "part1"
+    (is (= (part1 ".+8.\n.12.\n.*6.") 26))
+    (is (= (part1 example-input) 4361))
+    (is (= (part1 puzzle-input) 535351))
+    )
+  (testing "part2"
+    (is (= (part2 example-input) 467835))
+    (is (= (part2 puzzle-input) 87287096))
+    )
+  )
