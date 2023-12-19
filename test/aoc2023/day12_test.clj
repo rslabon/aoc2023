e00301df211ac74aa227dce19361827faf52f447
Rafal Slabon <rafal.slabon@ocado.com>
2023-12-19 09:59:28 +0100

    day 18 - part2 - done

--- test/aoc2023/day12_test.clj
+++ test/aoc2023/day12_test.clj
@@ -11,12 +11,13 @@
                (not= nil found)))))
 
 (def mem-replace-missing
-  (memoize (fn [line group offset]
-             (if (>= offset (count line))
+  (memoize (fn [line group offset end]
+             (println offset end)
+             (if (>= offset end)
                [line]
                (filterv #(matches? % group)
                         (sort (set
-                                (let [text (subs line offset)
+                                (let [text (subs line offset end)
                                       pre (.substring line 0 offset)
                                       p (re-pattern (str "(?<!#)[\\?|#]{" group "}(?!#)"))
                                       replaced (if (= 1 group)
@@ -31,19 +32,22 @@
                                                           (recur (rest s1) (rest s2) (inc idx))
                                                           ))
                                       ]
-                                  (cons replaced (mem-replace-missing line group replacement-idx))
+                                  (cons replaced (mem-replace-missing line group replacement-idx end))
                                   ))
                               ))))))
 
 (defn replace-missing
-  ([line group] (mem-replace-missing line group 0))
+  ([line group] (mem-replace-missing line group 0 (count line)))
+  ([line group end] (mem-replace-missing line group 0 end))
   )
 
 (defn find-arrangements
   [line groups]
   (let [p (map (fn [group] (str "[\\.\\?]*(?<!#)#{" group "}(?!#)[\\.\\?]*")) groups)
         p (str/join p)
-        p (str "^" p "$")]
+        p (str "^" p "$")
+        n (- (count line) (apply + groups) (inc (count groups)))
+        ]
     (filterv
       #(re-find (re-pattern p) %)
       (sort
@@ -55,7 +59,7 @@
               arrangements
               (let [group (first groups)]
                 (recur (rest groups)
-                       (mapcat #(replace-missing % group) arrangements)
+                       (mapcat #(replace-missing % group n) arrangements)
                        )
                 ))
             )
@@ -107,6 +111,56 @@
     (apply + arrangements-count)
     )
   )
+
+;@cache
+;def recurse(lava, springs, result=0):
+;    if not springs:
+;        return '#' not in lava
+;    current, springs = springs[0], springs[1:]
+;    for i in range(len(lava) - sum(springs) - len(springs) - current + 1):
+;        if "#" in lava[:i]:
+;            break
+;        if (nxt := i + current) <= len(lava) and '.' not in lava[i : nxt] and lava[nxt : nxt + 1] != "#":
+;            result += recurse(lava[nxt + 1:], springs)
+;    return result
+;with open("day12.txt", "r") as file:
+;    data = [x.split() for x in file.read().splitlines()]
+;    p1, p2 = 0, 0
+;    for e, (lava, springs) in enumerate(data):
+;        p1 += recurse(lava, (springs := tuple(map(int, springs.split(",")))))
+;        p2 += recurse("?".join([lava] * 5), springs * 5)
+;    print(p1, p2)
+
+(defn count-arrangements
+  ([line groups result]
+   (println groups)
+   (if (empty? groups)
+     (if (str/includes? line "#")
+       (do (println "0") 0)
+       (do (println "1") 1)
+       )
+     (let [current (first groups)
+           groups (rest groups)
+           positions (range (- (count line) (apply + groups) (count groups) (inc current)))
+           _ (println positions)]
+       (loop [positions positions
+              result result]
+         (let [i (first positions)]
+           (if (empty? positions)
+             result
+             (if (str/includes? (subs line 0 i) "#")
+               result
+               (let [next (+ i current)
+                     _ (println "next=" next)]
+                 (if (and (<= next (count line)) (not (str/includes? (subs line i next) ".")) (not (str/includes? (subs line next (inc next)) "#")))
+                   (recur (rest positions) (+ result (count-arrangements (subs line (inc next)) groups result)))
+                   (recur (rest positions) result)
+                   )
+                 ))
+             )
+           )
+         )
+       ))))
 
 (deftest day12-test
   (testing "matches?"
@@ -209,8 +263,11 @@
     )
   (testing "part1"
     (is (= (part1 example-input) 21))
-    (is (= (part1 puzzle-input) 7633))
+    ;(is (= (part1 puzzle-input) 7633))
     )
+  ;(testing "count-arrangements"
+  ;  (is (= (count-arrangements "????.######..#####." [1 6 5] 0) 4))
+  ;  )
   (testing "part2"
     ;(is (= (part2 example-input) 0))
     )
